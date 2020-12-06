6a435e5a55347f4e6284c6b93b28734b85fabc10
Blake Johnson <bjohn136@emich.edu>
2020-12-05 20:00:18 -0500

    Merged Sams and Blakes projects

--- Assets/Scripts/FirstPMovement.cs
+++ Assets/Scripts/FirstPMovement.cs
@@ -2,39 +2,22 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class FirstPMovement : MonoBehaviour
 {
-    public float speedH = 2.0f;
-    public float speedV = 2.9f;
-	public float moveSpeed = 50f;
+    public CharacterController controller;
+    public float speed = 12f;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
 
-	
-	// Update is called once per frame
-    void Update() {
-		CameraMove();
-		Move();
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
     }
-	
-	void CameraMove() {
-		yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-	}
-	
-	void Move () {
-		float xInput = Input.GetAxis("Horizontal");
-		float zInput = Input.GetAxis("Vertical");
-		transform.position += new Vector3(xInput * moveSpeed * Time.deltaTime, 0f, zInput * moveSpeed * Time.deltaTime);
-		
-		// TO_DO
-		// 'forward' direction should be the same direction that camera is facing
-	}
 }
-
--- Assets/Scripts/ThirdPMovement.cs
+++ Assets/Scripts/ThirdPMovement.cs
@@ -1,5 +1,7 @@
 ﻿/* This script controls movement, rotation, and zooming
-   of camera while in third person */
+
+  of camera while in third person */
+
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,76 +11,89 @@ using UnityEngine;
 
 public class ThirdPMovement : MonoBehaviour
 {
-    //public float speedH = 2.0f;
-    //public float speedV = 2.9f;
+    public float orthographicSizeMin;
+    public float orthographicSizeMax;
+    public float fovMin;
+    public float fovMax;
 
-    //private float yaw = 0.0f;
-    //private float pitch = 0.0f;
-	
-	public float moveSpeed;
-	public float zoomSpeed;
-	public float minZoomDist;
-	public float maxZoomDist;
-	public float rotateSpeed = 0.1f;
-	
-	Vector2 p1;
-	Vector2 p2;
-	
-	public Camera cam;
+    public float moveSpeed;
+    public float zoomSpeed;
+    public float rotateSpeed = 0.1f;
 
-    // Start is called before the first frame update
-    private void Awake() {
-		cam = Camera.main;
+
+    Vector2 p1;
+    Vector2 p2;
+
+    public Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
     }
-	
+
+
     // Update is called once per frame
-    void Update() {
-		// Actual movement
-		Move();
-		Rotate();
-		Zoom();
-    }
-	
-	// Movement with WASD keys
-	void Move () {
-		float xInput = Input.GetAxis("Horizontal");
-		float zInput = Input.GetAxis("Vertical");
-		transform.position += new Vector3(xInput * moveSpeed * Time.deltaTime, 0f, zInput * moveSpeed * Time.deltaTime);
-		
-		//TO_DO Update this method so that 'forward' moves in the direction that camera is facing
-	}
-	
-	// Uses middle mouse scroll button to rotate screen
-	void Rotate() {
-		if (Input.GetMouseButtonDown(2)) {
-			p1 = Input.mousePosition;
-		}
-		
-		if (Input.GetMouseButton(2)) {
-			p2 = Input.mousePosition;
-			
-			float dx = (p2 - p1).x * rotateSpeed * Time.deltaTime;
-			float dy = (p2 - p1).y * rotateSpeed * Time.deltaTime;
-			
-			transform.rotation *= Quaternion.Euler(new Vector3(0, dx, 0));
-			
-		}
-	}
-	
-	// Scroll wheel used to zoom in and out
-	void Zoom() {
-		float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-		float dist = Vector3.Distance(transform.position, cam.transform.position);
-		
-		// TO_DO - These should restrict how close/far you can zoom
-		// Currently broken, not sure why
-		//if (dist < minZoomDist && scrollInput > 0.0f)
-			//return;
-		//else if (dist > maxZoomDist && scrollInput < 0.0f)
-			//return;
-		
-		cam.transform.position += cam.transform.forward * scrollInput * zoomSpeed;
-			
-	}
-}
 
+    void Update()
+    {
+        // Actual movement
+        Move();
+        Rotate();
+        Zoom();
+    }
+
+    // Movement with WASD keys
+    void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(xInput * moveSpeed * Time.deltaTime, 0f, zInput * moveSpeed * Time.deltaTime);
+    }
+
+    // Uses middle mouse scroll button to rotate screen
+    void Rotate()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            p1 = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            p2 = Input.mousePosition;
+            float dx = (p2 - p1).x * rotateSpeed * Time.deltaTime;
+            float dy = (p2 - p1).y * rotateSpeed * Time.deltaTime;
+            transform.rotation *= Quaternion.Euler(new Vector3(0, dx, 0));
+        }
+    }
+
+
+    void Zoom()
+    {
+        if (cam.orthographic)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                cam.orthographicSize += zoomSpeed;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                cam.orthographicSize -= zoomSpeed;
+            }
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+        }
+        else
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                cam.fieldOfView += zoomSpeed;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                cam.fieldOfView -= zoomSpeed;
+            }
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, fovMin, fovMax);
+        }
+    }
+
+}
