15e5638956d290eaf96ab7689c331cb644e4c3a7
Blake Johnson <bjohn136@emich.edu>
2020-12-17 23:41:21 -0500

    Delete button

---
+++ Assets/Scripts/Delete.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour
+{
+    public string tag;
+    
+    public void DeleteObj(string tag)
+    {
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject target in gameObjects)
+        {
+            GameObject.Destroy(target);
+        }
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private MeshRenderer myRend;
+
+   void Start()
+    {
+        myRend = GetComponent<MeshRenderer>();
+    }
+
+    void ClickMe()
+    {
+        Destroy(myRend);
+    }
+}
